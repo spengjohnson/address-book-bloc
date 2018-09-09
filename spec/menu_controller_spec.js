aa504c87c29783c5d983d948e11552b44bd08028
spjohnson <spgant@gmail.com>
2018-09-09 16:56:30 -0600

    changes to menu controller spec

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -19,15 +19,15 @@ describe('MenuController', () => {
     })
     describe('#delete()', () => {
         it('should not remove any contacts that do not match the ID passed', (done) => {
-            this.book.addContact('Rick Deckard', '000-000-000', 'null@null.com')
+            this.menu.book.addContact('Rick Deckard', '000-000-000', 'null@null.com')
                 .then(() => {
-                    this.book.getContacts()
+                    this.menu.book.getContacts()
                         .then((contacts) => {
                             expect(contacts[0].name).toBe('Rick Dekard');
                             expect(contacts.length).toBe(1);
-                            this.book.delete(99)
+                            this.menu.book.delete(99)
                                 .then(() => {
-                                    this.book.getContacts()
+                                    this.menu.book.getContacts()
                                         .then((contacts) => {
                                             expect(contacts.length).toBe(1);
                                             done();
@@ -38,14 +38,14 @@ describe('MenuController', () => {
         });
 
         it('should remove the contact that matches the ID passed', (done) => {
-            this.book.addContact('Rick Deckard', '000-000-000', 'null@null.com').then((contact) => {
-                this.book.getContacts()
+            this.menu.book.addContact('Rick Deckard', '000-000-000', 'null@null.com').then((contact) => {
+                this.menu.book.getContacts()
                     .then((contacts) => {
                         expect(contacts[0].name).toBe('Rick Deckard');
                         exect(contacts.name).toBe(1);
-                        this.book.delete(contact.id)
+                        this.menu.book.delete(contact.id)
                             .then(() => {
-                                this.book.getContacts()
+                                this.menu.book.getContacts()
                                     .then((contacts) => {
                                         expect(contacts.length).toBe(0);
                                         done();
